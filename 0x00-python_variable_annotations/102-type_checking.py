4c0e5e92ba7724d51440ead6faab28261f7499c5
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-05 19:26:46 +0000

    use mypy

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -14,8 +14,9 @@ def zoom_array(lst: List, factor: int = 2) -> List:
     return zoomed_in
 
 
-array = [12, 72, 91]
+if __name__ == "__main__":
+    array = [12, 72, 91]
 
-zoom_2x = zoom_array(array)
+    zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3)
+    zoom_3x = zoom_array(array, 3)
