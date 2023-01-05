9396dfdce1c68d5c10ebb2692c40dbe6ba0ba93a
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-05 17:42:00 +0000

    test

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,9 +6,9 @@ from typing import List, Set, Dict, Tuple
 import typing
 
 
-def make_multiplier(n: float) -> typing.Callable[[float], float]:
+def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
     """ joins two strings """
     def f(x: float) -> float:
         """ multiplies n by a number from parent function """
-        return n * x
+        return multiplier * x
     return f
