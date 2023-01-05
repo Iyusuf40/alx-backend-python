565d1a704fa8df49519da94570b21925744c0736
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-05 16:42:10 +0000

    add file 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" module docs """
+
+
+from typing import List, Set, Dict, Tuple
+import typing
+
+
+def to_kv(k: str, r: typing.Union[int, float]) -> Tuple[str, float]:
+    """ joins two strings """
+    res = k, r*r
+
+    return res
