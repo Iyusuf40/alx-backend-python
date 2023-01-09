be868601deb6cd265c386300fc8e9052b4f96399
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-09 21:36:13 +0000

    add usage of asyncio.gather

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" module's doc string """
+
+
+import asyncio
+import random
+from typing import List, Callable
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wrapper(f: Callable, arg: int, lst: List[float]) -> None:
+    x = await f(arg)
+    lst.append(x)
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """demo of asyncio"""
+    coros = []
+    lst: List[float] = []
+    for _ in range(n):
+        coros.append(wrapper(wait_random, max_delay, lst))
+    await asyncio.gather(*coros)
+    return lst
