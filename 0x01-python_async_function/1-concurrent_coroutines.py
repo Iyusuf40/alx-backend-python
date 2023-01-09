c363f560b53479873c403090cdb4e377eab459c3
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-09 21:47:30 +0000

    add self note

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
-""" module's doc string """
+""" module contains functions that demonstrate
+coroutines with python's asyncio.
+NOTE:  the coroutines have to be bound to an event
+loop in order to work as expected. for example
+Using a for _ in range and awaiting will instead of work
+asynchroniously, work in a bocking manner.
+making a tuple of the coroutines and passing it to a
+asyncio.gather method ensures they are bound to an
+event loop and in that way it works as expected """
 
 
 import asyncio
@@ -11,6 +19,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wrapper(f: Callable, arg: int, lst: List[float]) -> None:
+    """ wrapper function that takes a coroutines and awats its
+    value before appending to a list"""
     x = await f(arg)
     lst.append(x)
 
