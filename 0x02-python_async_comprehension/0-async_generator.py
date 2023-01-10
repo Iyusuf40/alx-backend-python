bebdf1ac50f72dae6d7bf1db562a96aed7048d61
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-10 11:22:22 +0000

    add an async generator function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,11 +3,12 @@
 
 
 import asyncio
-from typing import List, Iterator, Generator
+from typing import List, Iterator, AsyncGenerator
 import random
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """ yields a random int """
     for i in range(10):
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
