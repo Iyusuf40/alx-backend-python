ba4a2f8c61436feaa628ec529cd042647dce4ac0
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-27 06:08:06 +0000

    add TestGithubOrgClient test case

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,7 +19,7 @@ from utils import (
     access_nested_map,
     memoize,
 )
-# from nose.tools import assert_equal
+from client import GithubOrgClient
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -91,6 +91,25 @@ class TestMemoize(unittest.TestCase):
             self.assertEqual(testInstance.a_property, 42)
             self.assertEqual(testInstance.a_property, 42)
             testInstance.a_method.assert_called_once
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ Tests GithubOrgClient """
+
+    @parameterized.expand([("google",),  ("abc",)])
+    def test_org(self, org):
+        """ tests org method of GithubOrgClient """
+        client_inst = GithubOrgClient(org)
+        # NOTE: see the patch target "client.get_json"
+        # Always the exact object is patched based on
+        # the module it resides in.
+        # patching utils.get_json will fail mocking because
+        # the get get_json object of interest is the one
+        # imported in client module
+        import client  # to assert it was called
+        with patch("client.get_json", return_value=org) as m_gt_json:
+            self.assertEqual(client_inst.org, org)
+            client.get_json.assert_called_once
 
 
 if __name__ == "__main__":
